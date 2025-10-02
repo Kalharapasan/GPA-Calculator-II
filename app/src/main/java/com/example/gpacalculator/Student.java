8143c76a1958dfae9f3a0d5ab83896d97a1cb0eb
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-02 20:50:39 +0530

    Cmplete Student.java

--- app/src/main/java/com/example/gpacalculator/Student.java
+++ app/src/main/java/com/example/gpacalculator/Student.java
@@ -1,4 +1,41 @@
 package com.example.gpacalculator;
-
 public class Student {
+    private long id;
+    private String name;
+    private String indexNumber;
+
+    public Student(String name, String indexNumber) {
+        this.name = name;
+        this.indexNumber = indexNumber;
+    }
+
+    public Student(long id, String name, String indexNumber) {
+        this.id = id;
+        this.name = name;
+        this.indexNumber = indexNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIndexNumber() {
+        return indexNumber;
+    }
+
+    public void setIndexNumber(String indexNumber) {
+        this.indexNumber = indexNumber;
+    }
 }
